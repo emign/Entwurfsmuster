4b08fee3e421914117f417d0a89e906d96993afb
gutenmorgenuhu <uhu@areaemig.de>
2017-04-04 13:25:14 +0200

    rename

--- src/pattern/View.java
+++ src/pattern/View.java
@@ -4,10 +4,13 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class View extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -24,10 +27,5 @@ public class Main extends Application {
         primaryStage.setTitle("Entwurfsmuster");
         primaryStage.setScene(new Scene(root, 700, 300));
         primaryStage.show();
-    }
-
-
-    public static void main(String[] args) {
-        launch(args);
     }
 }
